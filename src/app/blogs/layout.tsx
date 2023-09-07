9144d342a23eaf99efdffea2ac49c66e990aff3e
manupadev-asc <manupa.samarawickrama@ascentic.se>
2023-09-07 09:52:20 +0530

    e

--- src/app/blogs/layout.tsx
+++ src/app/blogs/layout.tsx
@@ -1,9 +1,9 @@
 function BlogsLayout({ children, interception }: any) {
   return (
-    <>
+    <div className="grid grid-cols-2">
       {children}
       {interception}
-    </>
+    </div>
   );
 }
 
--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 function BlogPage() {
   return (
     <div>
-        <h1>Blog Page</h1>
+      <h1>Blog Page</h1>
       <Link href="/categories/5/a/e">Categories</Link>
     </div>
   );
